e38b120c8629c135625901741691121fbad32276
Dino Hadzic <dhadzic@uw.edu>
2019-04-19 13:02:20 -0700

    Add files via upload

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+
+    public PlayerMovement movement;
+
+    public GameManager gameManager;
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+
+        }
+    }
+
+}
